065709804f03983033914b101017834edd642c51
majidzahedi <eemajidzahedi@hotmail.com>
2022-05-09 15:06:01 +0430

    FEAT: config for herodu deploy

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import { context } from "./context";
 
 import { schema } from "./schema";
@@ -6,9 +7,11 @@ import { schema } from "./schema";
 export const server = new ApolloServer({
   schema,
   context,
+  introspection: true, // 1
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
-const port = 4000;
+const port = process.env.PORT || 3000; // 3
 
 server.listen({ port }).then(({ url }) => {
   console.log(`:> Server is ready at ${url}`);
